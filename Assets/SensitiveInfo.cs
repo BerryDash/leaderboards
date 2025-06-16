40d398d06ca07380e5a7e6502b6cc5389af6f98c
Lncvrt <lncvrtreal@gmail.com>
2025-06-15 19:20:26 -0700

    1.0.3

--- Assets/SensitiveInfo.cs
+++ Assets/SensitiveInfo.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using UnityEngine;
 
 public class SensitiveInfo
 {
@@ -11,40 +12,56 @@ public class SensitiveInfo
 
     public static string Encrypt(string plainText, string key)
     {
-        using Aes aes = Aes.Create();
-        aes.Key = Encoding.UTF8.GetBytes(key);
-        aes.Mode = CipherMode.CBC;
-        aes.Padding = PaddingMode.PKCS7;
-        aes.GenerateIV();
-
-        using MemoryStream ms = new();
-        ms.Write(aes.IV, 0, aes.IV.Length);
-
-        using (var cryptoStream = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
-        using (var writer = new StreamWriter(cryptoStream))
+        try
         {
-            writer.Write(plainText);
-        }
+            using Aes aes = Aes.Create();
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.GenerateIV();
 
-        return Convert.ToBase64String(ms.ToArray());
+            using MemoryStream ms = new();
+            ms.Write(aes.IV, 0, aes.IV.Length);
+
+            using (var cryptoStream = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+            using (var writer = new StreamWriter(cryptoStream))
+            {
+                writer.Write(plainText);
+            }
+
+            return Convert.ToBase64String(ms.ToArray());
+        }
+        catch
+        {
+            Application.Quit();
+            return "-997"; //the server returns this if theres an issue with encryption/decryption so the client will too ig
+        }
     }
 
     public static string Decrypt(string dataB64, string key)
     {
-        var data = Convert.FromBase64String(dataB64);
-        using Aes aes = Aes.Create();
-        aes.Key = Encoding.UTF8.GetBytes(key);
-        aes.Mode = CipherMode.CBC;
-        aes.Padding = PaddingMode.PKCS7;
+        try
+        {
+            var data = Convert.FromBase64String(dataB64);
+            using Aes aes = Aes.Create();
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
 
-        byte[] iv = new byte[16];
-        Array.Copy(data, 0, iv, 0, iv.Length);
-        aes.IV = iv;
+            byte[] iv = new byte[16];
+            Array.Copy(data, 0, iv, 0, iv.Length);
+            aes.IV = iv;
 
-        using MemoryStream ms = new(data, iv.Length, data.Length - iv.Length);
-        using var cryptoStream = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read);
-        using StreamReader reader = new(cryptoStream);
+            using MemoryStream ms = new(data, iv.Length, data.Length - iv.Length);
+            using var cryptoStream = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read);
+            using StreamReader reader = new(cryptoStream);
 
-        return reader.ReadToEnd();
+            return reader.ReadToEnd();
+        }
+        catch
+        {
+            Application.Quit();
+            return "-997"; //the server returns this if theres an issue with encryption/decryption so the client will too ig
+        }
     }
 }
